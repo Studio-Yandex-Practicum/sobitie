801f3ca0adede43cec8813a3bdd840322c1158bf
Пользователь <flamepw11@yandex.ru>
2023-02-17 20:19:44 +0300

    add 4 button, add func start

---
+++ src/bot/services.py
@@ -0,0 +1,13 @@
+from telegram import Bot
+from telegram.ext import ApplicationBuilder
+from core.settings import TELEGRAM_TOKEN
+
+from bot.handlers.main_handler import conversation_handler
+
+
+def start_bot():
+    """Старт бота."""
+    bot = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+    bot.add_handler(conversation_handler)
+    return bot
--- src/core/settings.py
+++ src/core/settings.py
@@ -1,4 +1,10 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOG_LEVEL = 'INFO'
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
