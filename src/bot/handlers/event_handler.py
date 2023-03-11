9db7b85db1387c274ddc9afb3e5ec2a7bd20b2ba
Вячеслав Роев <roev96@yandex.ru>
2023-03-11 20:26:44 +0300

    Доработаны кнопки раздела Мероприятия

--- src/bot/handlers/event_handler.py
+++ src/bot/handlers/event_handler.py
@@ -1,10 +1,10 @@
 from telegram.ext import ConversationHandler, CallbackQueryHandler, CommandHandler
 
-from bot.convers_func.event_conversation import (get_events, get_perfomances, get_about_event, get_master_classes)
+from bot.convers_func.event_conversation import (get_events, get_perfomances, get_master_classes)
 from bot.convers_func.main_conversation import end
 from bot.keyboards.main import END, EVENTS
 from bot.keyboards.event import (GET_MASTER_CLASS,
-                                 GET_PERFORMANCES, GET_EVENT,
+                                 GET_PERFORMANCES,
                                  CHOOSE_EVENT)
 # from core.states import EVENTS
 
@@ -13,9 +13,8 @@ event_conv = ConversationHandler(
     entry_points=[CallbackQueryHandler(get_events, pattern='^' + EVENTS + '$')],
     states={
         CHOOSE_EVENT: [
-            CallbackQueryHandler(get_perfomances, pattern='^' + GET_MASTER_CLASS + '$'),
-            CallbackQueryHandler(get_about_event, pattern='^' + GET_PERFORMANCES + '$'),
-            CallbackQueryHandler(get_master_classes, pattern='^' + GET_EVENT + '$'),
+            CallbackQueryHandler(get_perfomances, pattern='^' + GET_PERFORMANCES + '$'),
+            CallbackQueryHandler(get_master_classes, pattern='^' + GET_MASTER_CLASS + '$'),
         ]
     },
     fallbacks=[CommandHandler(END, end)]
