c00bc777371f1fde515454ef20f2e3677253be75
Даниил Паутов <danpautoff@yandex.ru>
2023-03-15 19:21:00 +0300

    Configure admin panel for events.

--- drf_sobitie/event/admin.py
+++ drf_sobitie/event/admin.py
@@ -1,15 +1,64 @@
 from django.contrib import admin
+from django.template.defaultfilters import truncatechars
+from django.utils.safestring import mark_safe
 
-from event.models import Event, Category, Quote
+from event.models import Category, Event, Quote
+
+LENGTH_OF_QUOTE_DISPLAY = 200
 
 
 class QuoteAdmin(admin.ModelAdmin):
-    list_display = ('text', 'author', 'image', 'add_time') 
-    search_fields = ('text', 'author')
-    list_filter = ('add_time',)
-    empty_value_display = '-пусто-'
+    date_hierarchy = "add_time"
+    list_display = (
+        "id",
+        "short_text",
+        "author",
+        "thumbnail_of_image",
+        "add_time",
+    )
+    list_display_links = ("id", "short_text")
+    search_fields = ("text", "author")
+    empty_value_display = "-пусто-"
+
+    @admin.display(description="Изображение")
+    def thumbnail_of_image(self, obj):
+        """Отображает в Админ-панели миниатюру изображения."""
+        img_tag = self.empty_value_display
+        if obj.image:
+            img_tag = f'<img src="{obj.image.url}" style="max-height: 32px;">'
+        return mark_safe(img_tag)
+
+    @admin.display(description="Текст")
+    def short_text(self, obj):
+        """Укорачивает отображение цитаты в Админ-панели."""
+        return truncatechars(obj.text, LENGTH_OF_QUOTE_DISPLAY)
 
 
-admin.site.register(Event)
-admin.site.register(Category)
+class EventAdmin(admin.ModelAdmin):
+    date_hierarchy = "event_time"
+    list_display = (
+        "id",
+        "name",
+        "location",
+        "event_time",
+        "category",
+        "add_time",
+        "change_time",
+    )
+    list_display_links = ("id", "name")
+    search_fields = ("name", "location")
+    list_filter = ("category",)
+    empty_value_display = "-пусто-"
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    date_hierarchy = "add_time"
+    list_display = ("id", "name", "add_time", "change_time")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Quote, QuoteAdmin)
