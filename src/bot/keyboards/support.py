0eeaf6843eee84ea17b420bfb437db67ddd38ac4
Daniil Pautov <danpautoff@yandex.ru>
2023-03-26 19:52:20 +0300

    Refactor the "give support" section. (#57)
    * Fix link in button.
    
    * Add text to "Make a donation" section.
    
    * Add "Give support" menu text.
    
    * Fix code formatting.
    
    * Refactor souvenir purchasing logic and finalize section.
    
    * Fix code formatting.

--- src/bot/keyboards/support.py
+++ src/bot/keyboards/support.py
@@ -1,9 +1,9 @@
 import emoji
 from telegram import InlineKeyboardButton
 
+from bot.keyboards.event import NOTIFICATIONS_BUTTON
 from bot.keyboards.main import (
     GIVE_SUPPORT,
-    RETURN_BACK_BUTTON_TEXT,
     SHORT_RETURN_BACK_BUTTON_TEXT,
     SHORT_RETURN_TO_START_BUTTON_TEXT,
     create_return_to_start_button,
@@ -24,9 +24,6 @@ FOLLOW_US_TELEGRAM = "FOLLOW_US_TELEGRAM"
 CREATE_COLLECTION = "CREATE_COLLECTION"
 CASHBACK = "CASHBACK"
 
-# Константы для подменю "Заказать сувениры"
-CHARITY_FAIR = "CHARITY_FAIR"
-CORPORATE_FAIR = "CORPORATE_FAIR"
 
 RETURN_TO_SUPPORT_AND_RETURN_TO_START_BUTTONS = [
     [
@@ -35,26 +32,18 @@ RETURN_TO_SUPPORT_AND_RETURN_TO_START_BUTTONS = [
     ]
 ]
 
-# КНОПКИ МЕНЮ "ПОМОЧЬ"
 SUPPORT_MENU_BUTTONS = [
     [
         InlineKeyboardButton(
-            text=f"{emoji.emojize(':speech_balloon:')} Связь по вопросу помощи",
-            callback_data=COMMUNICATE_FOR_HELP,
+            text=f"{emoji.emojize(':speech_balloon:')} Связь по вопросу помощи", callback_data=COMMUNICATE_FOR_HELP
         )
     ],
     [
         InlineKeyboardButton(
-            text=f"{emoji.emojize(':credit_card:')} Сделать пожертвование",
-            callback_data=SHOW_DONATION_OPTIONS,
+            text=f"{emoji.emojize(':credit_card:')} Сделать пожертвование", callback_data=SHOW_DONATION_OPTIONS
         )
     ],
-    [
-        InlineKeyboardButton(
-            text=f"{emoji.emojize(':package:')} Заказать сувениры",
-            callback_data=ORDER_SOUVENIRS,
-        )
-    ],
+    [InlineKeyboardButton(text=f"{emoji.emojize(':package:')} Приобрести сувениры", callback_data=ORDER_SOUVENIRS)],
     [
         InlineKeyboardButton(
             text=f"{emoji.emojize(':handshake::money_bag:')} Создать сбор", callback_data=CREATE_COLLECTION
@@ -63,44 +52,24 @@ SUPPORT_MENU_BUTTONS = [
     [InlineKeyboardButton(text=f"{emoji.emojize(':money_with_wings:')} Подключить кешбэк", callback_data=CASHBACK)],
     [
         InlineKeyboardButton(
-            text=f"{emoji.emojize(':mobile_phone_with_arrow:')} Стать активным подписчиком",
-            callback_data=FOLLOW_US,
+            text=f"{emoji.emojize(':mobile_phone_with_arrow:')} Стать активным подписчиком", callback_data=FOLLOW_US
         )
     ],
     [create_return_to_start_button()],
 ]
 
-# КНОПКИ ПОДМЕНЮ "ВЫБРАТЬ СПОСОБ ПОЖЕРТВОВАНИЯ"
 DONATION_OPTIONS_MENU_BUTTONS = [
     [InlineKeyboardButton(text="Форма на сайте", url="https://sobytie.center/howtohelp/")],
+    [
+        InlineKeyboardButton(
+            text='Благотворительный фонд "Нужна помощь"', url="https://nuzhnapomosh.ru/funds/centr-sobytie/"
+        )
+    ],
+    [InlineKeyboardButton(text='Подписка "Рубль в день"', url="https://365.nuzhnapomosh.ru/")],
     [InlineKeyboardButton(text="Тинькофф", url="https://www.tinkoff.ru/payments/provider-sobytie/")],
-    [
-        InlineKeyboardButton(
-            text="Разовое или регулярное пожертвование",
-            url="https://nuzhnapomosh.ru/funds/centr-sobytie/",
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='Благотворительный фонд "Нужна помощь"',
-            url="https://nuzhnapomosh.ru/funds/centr-sobytie/",
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text='Подписка "Рубль в день"',
-            url="https://nuzhnapomosh.ru/funds/centr-sobytie/",
-        )
-    ],
-    [
-        InlineKeyboardButton(
-            text=RETURN_BACK_BUTTON_TEXT,
-            callback_data=GIVE_SUPPORT,
-        )
-    ],
+    *RETURN_TO_SUPPORT_AND_RETURN_TO_START_BUTTONS,
 ]
 
-# кнопки для подписок на соцсети
 SUPPORT_FOLLOW_BUTTONS = [
     [
         InlineKeyboardButton(
@@ -119,8 +88,7 @@ SUPPORT_FOLLOW_BUTTONS = [
     *RETURN_TO_SUPPORT_AND_RETURN_TO_START_BUTTONS,
 ]
 
-MENU_ORDER_SUVENIR = [
-    [InlineKeyboardButton(text="Благотворительная ярмарка", callback_data=CHARITY_FAIR)],
-    [InlineKeyboardButton(text="Корпоративные подарки", callback_data=CORPORATE_FAIR)],
-    [InlineKeyboardButton(text=RETURN_BACK_BUTTON_TEXT, callback_data=GIVE_SUPPORT)],
+MENU_ORDER_SOUVENIR = [
+    [NOTIFICATIONS_BUTTON],
+    *RETURN_TO_SUPPORT_AND_RETURN_TO_START_BUTTONS,
 ]
