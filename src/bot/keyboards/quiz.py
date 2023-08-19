815a3f68682f11b692559cac3888bada43f9c274
Ilona Petina <ilonka19901996@yandex.ru>
2023-08-19 20:07:47 +0300

    fix_107_quiz_buttons_return

--- src/bot/keyboards/quiz.py
+++ src/bot/keyboards/quiz.py
@@ -1,6 +1,9 @@
 from telegram import InlineKeyboardButton
 
-from bot.keyboards.main import create_return_to_start_button
+from bot.keyboards.main import (
+    INTERACTIVE_GAME,
+    SHORT_RETURN_BACK_BUTTON_TEXT
+)
 
 START_QUIZZES = 'START_QUIZZES'
 START_QUESTIONS = "START_QUESTIONS"
@@ -8,9 +11,19 @@ NEXT_QUESTIONS = "NEXT_QUESTIONS"
 
 QUESTIONS_MENU_BUTTON = [
     [InlineKeyboardButton(text="Следующий вопрос.", callback_data=NEXT_QUESTIONS)],
-    [create_return_to_start_button(),]
+    [
+        InlineKeyboardButton(
+            text=SHORT_RETURN_BACK_BUTTON_TEXT,
+            callback_data=INTERACTIVE_GAME,
+        )
+    ],
 ]
 
 FINISH_QUIZ_MENU_BUTTON = [
-    [create_return_to_start_button(),]
+    [
+        InlineKeyboardButton(
+            text=SHORT_RETURN_BACK_BUTTON_TEXT,
+            callback_data=INTERACTIVE_GAME,
+        )
+    ],
 ]
