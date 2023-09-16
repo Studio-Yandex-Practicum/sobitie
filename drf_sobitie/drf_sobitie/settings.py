1cad81f2c03a1a6bd2dc077f6a2ad8b44c6fdd12
Данил Уткин <duck.danil@yandex.ru>
2023-09-16 19:59:46 +0300

    refactor env constants

--- drf_sobitie/drf_sobitie/settings.py
+++ drf_sobitie/drf_sobitie/settings.py
@@ -8,6 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
+VK_USER_KEY = os.getenv("VK_USER_KEY")
+VK_GROUP_ID = int(os.getenv("VK_GROUP_ID"))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
--- drf_sobitie/vk_con/vk_connect.py
+++ drf_sobitie/vk_con/vk_connect.py
@@ -1,13 +1,11 @@
-import os
-
 import requests
 import vk_api
 from dotenv import load_dotenv
 from vk_api.bot_longpoll import VkBotLongPoll
 
+from drf_sobitie.drf_sobitie.settings import VK_USER_KEY, VK_GROUP_ID
+
 load_dotenv()
-VK_USER_KEY = os.getenv("VK_USER_KEY")
-VK_GROUP_ID = int(os.getenv("VK_GROUP_ID"))
 
 
 def longpoll_vk():
