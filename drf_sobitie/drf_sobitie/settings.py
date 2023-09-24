73fa42e71a32ab8f337641ee6090667a6432a7ca
ialeks017 <108348616+ialeks017@users.noreply.github.com>
2023-09-24 18:36:44 +0300

    Убрал url-ы из настроек

--- drf_sobitie/drf_sobitie/settings.py
+++ drf_sobitie/drf_sobitie/settings.py
@@ -12,10 +12,6 @@ FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 LOG_LEVEL = "INFO"
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-EVENTS_URL = os.getenv("EVENTS_URL")
-QUOTE_URL = os.getenv("QUOTE_URL")
-STICKERPACK_URL = os.getenv("STICKERPACK_URL")
-QUIZZES_URL = os.getenv("QUIZZES_URL")
 NOTIFICATIONS_API_URL = os.getenv("NOTIFICATIONS_API_URL")
 CHECK_FOR_SUBSCRIPTION_API_URL = os.getenv("CHECK_FOR_SUBSCRIPTION_API_URL")
 HOST = os.getenv("HOST", "http://localhost:8000")
