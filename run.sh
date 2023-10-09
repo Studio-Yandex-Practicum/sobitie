3f75f72eb32678cfdf3ee407d398b6b674dc1dfd
ialeks017 <108348616+ialeks017@users.noreply.github.com>
2023-10-09 20:24:08 +0300

    Поправил deploy: прописал все миграции для котейнеров web и bot

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 docker-compose up -d --build
-docker-compose exec web python drf_sobitie/manage.py makemigrations
+docker-compose exec web python drf_sobitie/manage.py makemigrations event
+docker-compose exec web python drf_sobitie/manage.py makemigrations quiz
+docker-compose exec web python drf_sobitie/manage.py makemigrations sticker_pack
 docker-compose exec web python drf_sobitie/manage.py migrate --no-input
 docker-compose exec web python drf_sobitie/manage.py collectstatic --no-input
+docker-compose exec bot python drf_sobitie/manage.py makemigrations event
+docker-compose exec bot python drf_sobitie/manage.py makemigrations quiz
+docker-compose exec bot python drf_sobitie/manage.py makemigrations sticker_pack
+docker-compose exec bot python drf_sobitie/manage.py migrate --no-input
+docker-compose exec bot python drf_sobitie/manage.py collectstatic --no-input
