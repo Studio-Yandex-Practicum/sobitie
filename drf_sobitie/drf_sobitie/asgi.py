3f64203b2fdd7dd0eee25204b5e6b601287b5cf7
Андрей Тарасов <baby-shit@yandex.ru>
2023-11-18 19:01:33 +0300

    сделал в одной ветке все таски

--- drf_sobitie/drf_sobitie/asgi.py
+++ drf_sobitie/drf_sobitie/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for drf_sobitie project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from notifications.consumers import EventNotificationConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drf_sobitie.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter([
+        path(r'ws/send-event-notification/$', EventNotificationConsumer.as_asgi()),
+    ]),
+})
