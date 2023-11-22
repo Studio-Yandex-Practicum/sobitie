fd2251943b417a186f1d3d1d3fae63bb57d2f778
Todvaa <aa.varlamov1997@gmail.com>
2023-11-22 20:24:39 +0530

    refactoring and fix imports

--- drf_sobitie/conf/asgi.py
+++ drf_sobitie/conf/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from notifications.consumers import EventNotificationConsumer
+from drf_sobitie.notifications.consumers import EventNotificationConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
 
